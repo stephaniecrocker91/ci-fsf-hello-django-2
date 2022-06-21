a616f4684726260a3600a56dd7480f70819a20fb
Stephie <stephaniecrocker91@gmail.com>
2022-06-21 17:22:36 +0000

    Prepare to deploy to Heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,12 +76,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.parse('postgres://ajhfokkverijhf:3e35a711cb8270020fb5a80493e6701cd06694d63b98b1e377b4f4947324595b@ec2-34-200-35-222.compute-1.amazonaws.com:5432/d6o8hhr2aidjdr'),
     }
-}
 
 
 # Password validation
--- todo/test_views.py
+++ todo/test_views.py
@@ -46,4 +46,3 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertEqual(updated_item, 'Updated Name')
-  
